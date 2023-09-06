6cd510dc38281d2fe7dee362568f42f22bb29621
akhilx09 <79153017+akhilx09@users.noreply.github.com>
2023-09-06 16:32:46 +0530

    Add files via upload

---
+++ Arrposre.java
@@ -0,0 +1,13 @@
+//Java Program to print the elements of an array in reverse order
+
+
+public class Arrposre
+{
+        public static void main(String[] args){
+               int xy[]=new int[]{0,1,2,3,4,5,6,7,8,9};
+               for(int i=9;i>-1;i--){
+            
+                          System.out.println(xy[i]);
+}
+}
+}
