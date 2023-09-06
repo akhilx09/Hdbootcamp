6cf86a6548279b8278aa812df1599015f75c82e8
akhilx09 <79153017+akhilx09@users.noreply.github.com>
2023-09-06 16:35:32 +0530

    Add files via upload

---
+++ Arrposo.java
@@ -0,0 +1,12 @@
+//Java Program to print the elements of an array present on an odd position
+
+public class Arrpos{
+        public static void main(String[] args){
+               int xy[]=new int[]{0,1,2,3,4,5,6,7,8,9};
+               for(int i=0;i<11;i++){
+                   if(i%2!=0){
+                          System.out.println(xy[i]);
+}
+}
+}
+}
