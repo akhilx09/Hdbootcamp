277869b81a097f2b3bc2a8f47eef20ac888277d3
akhilx09 <79153017+akhilx09@users.noreply.github.com>
2023-09-06 16:12:46 +0530

    Add files via upload

---
+++ Arrev.java
@@ -0,0 +1,11 @@
+// Java Program to print the elements of an array
+
+public class Arrev{
+        public static void main(String[] args){
+               int earry[]=new int[]{1,2,3,4,5,6,7,8,9,10},i=0;
+               for(i=0;i<=11;i++){
+                  if(i%2==0){
+                    System.out.println(earry[i]);}
+               }
+}
+}
